d82400df7d09e859fa9237dccf53765d46ed1a99
Diego Vinicius <diegowebd@hotmail.com>
2017-03-10 13:50:43 -0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,11 +1,11 @@
-function pad(num) {
+var pad = (num) => {
     let size = 2;
     var s = num+"";
     while (s.length < size) s = "0" + s;
     return s;
 }
 
-module.exports = function(req, res, next){
+module.exports = (req, res, next) => {
     var start = +new Date();
     var stream = process.stdout;
     var url = req.url;
@@ -16,7 +16,7 @@ module.exports = function(req, res, next){
     var currentdate = new Date();
     var datetime = `[${currentdate.getDate()}/${(pad(currentdate.getMonth()+1))}/${currentdate.getFullYear()} ${pad(currentdate.getHours())}:${pad(currentdate.getMinutes())}:${pad(currentdate.getSeconds())}]`
 
-    res.on('finish', function() {
+    res.on('finish', () => {
         var duration = +new Date() - start;
         var message = `${datetime}  "${method} ${path} ${protocol}/${protocolVersion}" ${res.statusCode} - took ${duration} ms\n`
         stream.write(message);
